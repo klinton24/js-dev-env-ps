5335c8bfe8ed89f6003273bd46494237ca715269
K Smith <klinton.jp@gmail.com>
2018-08-21 13:31:11 -0700

    Implemented mock data generator and mock data schema.

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,4 +1,7 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
     return get('users');
@@ -9,12 +12,12 @@ export function deleteUser(id) {
 }
 
 function get(url) {
-    return fetch(url + url).then(onSuccess, onError);
+    return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
 // Can't call func delete since reserved word.
 function del(url) {
-    const request = new Request(url + url, {
+    const request = new Request(baseUrl + url, {
         method: 'DELETE'
     });
 
